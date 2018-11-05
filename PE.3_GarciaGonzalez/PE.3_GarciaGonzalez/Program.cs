74a3cf0b7006a71a098278fd850042ea18125b1b
ggerick <erick210205>
2018-11-04 21:02:06 -0800

    done
    no supe hacer el primero jojo

---
+++ PE.3_GarciaGonzalez/PE.3_GarciaGonzalez/Program.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace PE._GarciaGonzalez
+{
+   public class Tarea
+    {
+        public int id = 0;
+        public string nombre = "";
+        public string descripcion = "";
+        public string fechaInicio = "";
+        public string fechaFin = "";
+        public string status = "";
+
+    }
+
+    public class Operaciones
+    {
+       
+        List<Tarea> tareasGlobal = new List<Tarea>();//Declaracion de listas de objetos
+        List<Tarea> tareasPendientes = new List<Tarea>();
+        List<Tarea> tareasProceso = new List<Tarea>();
+        List<Tarea> tareasTerminadas = new List<Tarea>();
+        public void AgregarTareas()
+        {
+            Console.WriteLine("\t\tAgrega tareas\n\n");
+            Tarea tarea;
+            char opc = ' ';
+            int contador = 0;
+           
+            do//Entrada de datos pura y dura
+            {
+
+                tarea = new Tarea();//Instanciamos un nuevo ojbeto 'tarea'
+                tarea.id = contador + 1;
+                Console.Write("\nNombre de la tarea: ");
+                tarea.nombre = Console.ReadLine();
+                Console.Write("Agregar una descripcion para la tarea: ");
+                tarea.descripcion = Console.ReadLine();
+                Console.Write("Fecha de inicio(dd/MM/yy): ");
+                tarea.fechaInicio = Console.ReadLine();
+                Console.Write("Fecha esperada para terminar(dd/MM/yy): ");
+                tarea.fechaFin = Console.ReadLine();
+                tarea.status = "pendiente";
+                tareasGlobal.Add(tarea);
+                tareasPendientes.Add(tarea);//y con sus propeidades llenadas, lo mandamos a la lista correspondiente
+                Console.Write("\n\nDesea agregar otra tarea? No<S> \tSi<Cualquier otra tecla>");
+                opc = Console.ReadKey().KeyChar;
+                contador++; 
+
+            } while (opc != 's');//Para decidir cuantas tareas se van a añadir
+
+            Menu();//Llama al menu para realizar otra accion
+
+        }
+
+        public void VerTareas()
+        {
+            Console.Clear();
+            Console.WriteLine("\t\tLista tareas: ");
+            int opc = 0;
+            char salida = ' ';
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Que tareas deseas ver");//Un menú donde puede ver el contenido de las 3 listas
+                Console.WriteLine("1.- Tienes  " + tareasPendientes.Count + " tareas pendientes");
+                Console.WriteLine("2.- Tienes  " + tareasProceso.Count + " tareas en proceso");
+                Console.WriteLine("3.- Tienes  " + tareasTerminadas.Count + " tareas terminadas");
+                opc = int.Parse(Console.ReadLine());
+                switch(opc)
+                {
+                    case 1:
+                        Console.Clear();
+                        Console.WriteLine("\t\tTareas pendientes");
+                        foreach (var item in tareasPendientes)//Un simple despliegue de los elementos de la lista seleccionada
+                        {
+                            Console.WriteLine(item.id);
+                            Console.WriteLine(item.nombre);
+                            Console.WriteLine(item.descripcion);
+                            Console.WriteLine(item.fechaInicio);
+                            Console.WriteLine(item.fechaFin);
+                            Console.WriteLine(item.status);
+                        }
+
+                        break;
+
+                    case 2:
+                        Console.Clear();
+                        Console.WriteLine("\t\tTareas en proceso");//Un simple despliegue de los elementos de la lista seleccionada
+                        foreach (var item in tareasProceso)
+                        {
+                            Console.WriteLine(item.id);
+                            Console.WriteLine(item.nombre);
+                            Console.WriteLine(item.descripcion);
+                            Console.WriteLine(item.fechaInicio);
+                            Console.WriteLine(item.fechaFin);
+                            Console.WriteLine(item.status);
+                        }
+
+                        break;
+
+                    case 3:
+                        Console.Clear();
+                        Console.WriteLine("\t\tTareas terminadas");//Un simple despliegue de los elementos de la lista seleccionada
+                        foreach (var item in tareasTerminadas)
+                        {
+                            Console.WriteLine(item.id);
+                            Console.WriteLine(item.nombre);
+                            Console.WriteLine(item.descripcion);
+                            Console.WriteLine(item.fechaInicio);
+                            Console.WriteLine(item.fechaFin);
+                            Console.WriteLine(item.status);
+                        }
+
+                        break;
+
+                  
+
+                }
+
+                Console.Write("\nPara salir, presiona otra tecla que no sea s");
+                salida = Console.ReadKey().KeyChar;
+
+                
+            } while (salida == 's');//Sale del menu de VerTareas
+
+            Menu();
+        }
+
+        public void MoverTareas()//Aqui es donde comienza la magia - Un metodo para mover las tareas de una lista a otra
+        {
+            Console.WriteLine("\n\t\tAqui podras cambiar el estado de tus tareas (proceso/terminadas)");
+
+            int opc = 0;
+            string move = " ";
+            char opcion = ' ';
+            do
+            {
+
+                Console.Write("Elige una opcion: ");// Se elige de qué lista a qué otra lista se va a mover la tarea
+                Console.WriteLine("\n" +
+                                  "1.- Tareas Pendientes => Tareas en proceso");
+                Console.WriteLine("2.- Tareas en proceso => Tareas terminadas");
+                Console.WriteLine("3.- Tareas terminadas => Tareas en proceso");
+                opc = int.Parse(Console.ReadLine());
+                switch (opc)
+                {
+                    case 1:
+                        Console.Write("Escriba el nombre de la tarea que quiera mover de la lista pendientes: ");
+                        move = Console.ReadLine();//Preguntamos y almacenamos el nombre de la tarea en la variable 'move'
+                        var movible = tareasPendientes.Where(x => x.nombre == move);//Creamos una variable tipo var, que va a guardar el objeto cuando encuentre un valor igual a move en la lista.nombre
+                        tareasProceso.AddRange(movible);//Agregamos el objeto a la lista tareaProceso
+                        int index = tareasPendientes.FindIndex(a => a.nombre == move);//Aqui hacemos una busca del mismo nombre pero en vez de alcamenar el objeto, almacenará la posición de éste
+                        tareasPendientes.RemoveAt(index);//Y una vez encontrado, lo va a eliminar de la lista de donde se movió
+
+                        tareasProceso[index].status = "En proceso";//Le cambiamos el status a la tarea porque se cambio de lista
+                        break;
+
+                    case 2:
+                        Console.Write("Escriba el nombre de la tarea que quiera mover de la lista proceso: ");
+                        move = Console.ReadLine();
+                        var movible2 = tareasProceso.Where(x => x.nombre == move);
+                        tareasTerminadas.AddRange(movible2);
+                        int index2 = tareasProceso.FindIndex(a => a.nombre == move);
+                        tareasProceso.RemoveAt(index2);
+                        tareasTerminadas[index2].status = "Terminada"; 
+                        break;
+
+                    case 3:
+                        Console.Write("Escriba el nombre de la tarea que quiera mover de la lista terminada a proceso: ");
+                        move = Console.ReadLine();
+                        var movible3 = tareasTerminadas.Where(x => x.nombre == move);
+                        tareasProceso.AddRange(movible3);
+                        int index3 = tareasTerminadas.FindIndex(a => a.nombre == move);
+                        tareasTerminadas.RemoveAt(index3);
+                        tareasTerminadas[index3].status = "En proceso";
+                        break;
+
+
+
+
+                }
+                Console.Write("Si deseas seguir moviendo tareas, pulsa n. De lo contrario pulsa otra tecla ");
+               opcion = Console.ReadKey().KeyChar;
+            } while (opcion == 'n');
+            Menu();
+        }
+
+        public void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("\t\tMenu de tareas");
+            Console.WriteLine("1.- Agregar tareas");
+            Console.WriteLine("2.- Visualizar las tareas");
+            Console.WriteLine("3.- Marcar tareas como terminaras/en proceso de");
+            Console.WriteLine("4.- Salir del programa");
+            int opc = int.Parse(Console.ReadLine());
+
+
+            switch(opc)
+            {
+                case 1:
+                    AgregarTareas();
+                    break;
+
+                case 2:
+                    VerTareas();
+                    break;
+
+                case 3:
+                    MoverTareas();
+                    break;
+
+                case 4:
+                    break;
+
+                default:
+                    Console.WriteLine("Opcion invalida, vuelve a intentarlo");
+                    break;
+
+
+
+            }
+        }
+    }
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Operaciones ops = new Operaciones();
+            ops.Menu();
+            Console.ReadKey();
+
+           
+           
+           
+        }
+    }
+}
---
+++ lasVacas/lasVacas/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lasVacas
+{
+    public class Cows//Creamos la clase vaca, y cada vaca tendra nombre y tiempo(en cruzar el puente)
+    {
+        public string name;//Agregamos la  propiedad nombre
+        public int time;//Agregamos la proviedad de tiempo
+
+        public Cows(string name, int time)//Un constructor que recibe valores en los parametros
+        {
+            this.name = name;//Usamos "this" para usar el mismo nombre que los parametros
+            this.time = time;
+        }
+
+    }
+
+    public class Quizz
+    {
+        public List<Cows> mainBridge = new List<Cows>();//Creamos una lista de objetos vaca, que es el puente Inicial
+        public List<Cows> finalBridge = new List<Cows>();//Creamos una lista de objetos vaca, y es el final, donde pasaremos las vacas
+        int totalTime = 0;// el tiempo que ha pasado moviendo vacas
+
+        public void Values()
+        {
+            mainBridge.Add(new Cows("Mazie", 2));//Agregamos a Mazie
+            mainBridge.Add(new Cows("Daisy", 4));//Agregamos a Daisy
+            mainBridge.Add(new Cows("Crazy", 10));//Agregamos a Crazy
+            mainBridge.Add(new Cows("Lazy", 20));//Agregamos a Lazy
+        }
+
+        public void MovingCows()
+        {
+            Console.WriteLine("1.- Supongamos que Bob tiene cuatro vacas que quiere cruzar por un puente, pero solo un yugo," +
+                              " que puede sostener hasta dos vacas, lado a lado, atadas al yugo. El yugo es demasiado pesado para que lo lleve a través del puente," +
+                              " pero puede atar (y desatar) vacas a él en muy poco tiempo. De sus cuatro vacas, Mazie puede cruzar el puente en 2 minutos," +
+                              " Daisy puede cruzarlo en 4 minutos, Crazy puede cruzarlo en 10 minutos y Lazy puede cruzar en 20 minutos. Por supuesto," +
+                              " cuando dos vacas están atadas al yugo, deben ir a la velocidad de la vaca más lenta. Describe cómo Bob puede conseguir que todas" +
+                              "sus vacas crucen el puente en 34 minutos.");
+            Console.WriteLine("\n\n\t\tResolviendo\n\n");
+            totalTime = totalTime + mainBridge[1].time;
+            Console.WriteLine("Primero cruzan Mazie y Daisy al final\t{0} minutos", totalTime);
+            Console.ReadKey();
+            finalBridge.Add(mainBridge[0]);//Agregamos a Mazie al puente final
+            finalBridge.Add(mainBridge[1]);//Agregamos a Daisy al puente final
+            mainBridge.RemoveRange(0, 2);//Removemos a Mazie y Daisy
+            totalTime = totalTime + finalBridge[0].time;//Sumanos los tiempos Mazie y Daisy
+            Console.WriteLine("\bDespues Mazie vuelve al principio\t{0} minutos", totalTime); //Imprimimos el tiempo
+            Console.ReadKey();
+            mainBridge.Insert(0, finalBridge[0]);//Agregamos a Mazie al puente principal a la primera posicion de la lista
+            finalBridge.RemoveAt(0);//Removemos Mazie del puente final porque se devolvio
+            finalBridge.Add(mainBridge[1]);//Agregamos a Crazy al puente final
+            finalBridge.Add(mainBridge[2]);//Agregamos a Lazy al puente final
+            mainBridge.RemoveRange(1, 2);//Removemos a Crazy y Lazy del puente principal
+            totalTime = totalTime + finalBridge[2].time;//Sumamos los tiempos
+            Console.WriteLine("\bAhora cruza Crazy y Lazy al final\t{0} minutos", totalTime);
+            Console.ReadKey();
+            mainBridge.Add(finalBridge[0]);//Agregamos a Daisy al inicio
+            finalBridge.RemoveAt(0);//Removemos a Daisy del puente final porque se devuelve al principio
+            totalTime = totalTime + mainBridge[1].time;//Sumamos el tiempo de Daisy
+            Console.WriteLine("\bDaisy vuelve al principio\t\t{0} minutos", totalTime);//Imprimimos tiempos
+            Console.ReadKey();
+            finalBridge.Add(mainBridge[0]);
+            finalBridge.Add(mainBridge[1]);
+            mainBridge.Clear();
+            totalTime = totalTime + finalBridge[3].time;
+            Console.WriteLine("\bFinalmente cruzan Mazie y Daisy\t\t{0} minutos", totalTime);
+        }
+    }
+
+    public
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Quizz pop = new Quizz();
+            pop.Values();
+            pop.MovingCows();
+            Console.ReadKey();
+        }
+    }
+}
